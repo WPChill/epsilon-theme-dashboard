612f419bb6beb9a75407c9b0cc0a47d85d1144fa
c0sm1n87 <cristea.cosmin87@gmail.com>
2018-05-30 09:43:01 +0300

    Increase security in the theme dashboard controller as well. Actions must come from a super user.

--- inc/misc/class-epsilon-dashboard-ajax.php
+++ inc/misc/class-epsilon-dashboard-ajax.php
@@ -20,7 +20,7 @@ class Epsilon_Dashboard_Ajax {
 		) );
 		add_action( 'wp_ajax_nopriv_epsilon_dashboard_ajax_callback', array(
 			$this,
-			'epsilon_dashboard_ajax_callback',
+			'not_logged_notice',
 		) );
 	}
 
@@ -32,40 +32,11 @@ class Epsilon_Dashboard_Ajax {
 			$_POST['args'] = json_decode( wp_unslash( $_POST['args'] ), true );
 		}
 
-		if ( isset( $_POST['args'], $_POST['args']['nonce'] ) && ! wp_verify_nonce( sanitize_key( $_POST['args']['nonce'] ), 'epsilon_dashboard_nonce' ) ) {
-			wp_die(
-				wp_json_encode(
-					array(
-						'status' => false,
-						'error'  => esc_html__( 'Not allowed', 'epsilon-framework' ),
-					)
-				)
-			);
-		}
-
+		$this->_check_nonce();
+		$this->_check_user_role();
 		$args_action = array_map( 'sanitize_text_field', wp_unslash( $_POST['args']['action'] ) );
-
-		if ( count( $args_action ) !== 2 ) {
-			wp_die(
-				wp_json_encode(
-					array(
-						'status' => false,
-						'error'  => esc_html__( 'Not allowed', 'epsilon-framework' ),
-					)
-				)
-			);
-		}
-
-		if ( ! class_exists( $args_action[0] ) ) {
-			wp_die(
-				wp_json_encode(
-					array(
-						'status' => false,
-						'error'  => esc_html__( 'Class does not exist', 'epsilon-framework' ),
-					)
-				)
-			);
-		}
+		$this->_check_structure( $args_action );
+		$this->_check_class( $args_action[0] );
 
 		$class  = $args_action[0];
 		$method = $args_action[1];
@@ -78,27 +49,117 @@ class Epsilon_Dashboard_Ajax {
 		$response = $class::$method( $args );
 
 		if ( is_array( $response ) ) {
-			wp_die( wp_json_encode( $response ) );
+			$this->send_response( $response );
 		}
 
 		if ( 'ok' === $response ) {
-			wp_die(
-				wp_json_encode(
-					array(
-						'status'  => true,
-						'message' => 'ok',
-					)
+			$this->send_response(
+				array(
+					'status'  => true,
+					'message' => 'ok',
 				)
 			);
 		}
 
-		wp_die(
-			wp_json_encode(
-				array(
-					'status'  => false,
-					'message' => 'nok',
-				)
+		$this->send_response(
+			array(
+				'status'  => false,
+				'message' => 'nok',
 			)
 		);
+	}
+
+	/**
+	 * Not logged in
+	 *
+	 * @return string
+	 */
+	public static function not_logged_notice() {
+		return esc_html__( 'You must be logged in to do that!', 'epsilon-framework' );
+	}
+
+	/**
+	 * Checks nonce
+	 *
+	 * @return bool
+	 */
+	private function _check_nonce() {
+		if ( isset( $_POST['args'], $_POST['args']['nonce'] ) && ! wp_verify_nonce( sanitize_key( $_POST['args']['nonce'] ), 'epsilon_nonce' ) ) {
+
+			$this->send_response(
+				array(
+					'status' => false,
+					'error'  => esc_html__( 'Not allowed', 'epsilon-framework' ),
+				)
+			);
+		}
+
+		return true;
+	}
+
+	/**
+	 * Check if class exists
+	 *
+	 * @param string $class
+	 *
+	 * @return bool
+	 */
+	private function _check_class( $class = '' ) {
+		if ( ! class_exists( $class ) ) {
+			$this->send_response(
+				array(
+					'status' => false,
+					'error'  => esc_html__( 'Class does not exist', 'epsilon-framework' ),
+				)
+			);
+		}
+
+		return true;
+	}
+
+	/**
+	 * Check array structure
+	 *
+	 * @param $action
+	 *
+	 * @return bool
+	 */
+	private function _check_structure( $action ) {
+		if ( count( $action ) !== 2 ) {
+			$this->send_response(
+				array(
+					'status' => false,
+					'error'  => esc_html__( 'Not allowed', 'epsilon-framework' ),
+				)
+			);
+		}
+
+		return true;
+	}
+
+	/**
+	 * Check user role
+	 */
+	private function _check_user_role() {
+		$super_admin = is_super_admin( get_current_user_id() );
+		if ( ! $super_admin ) {
+			$this->send_response(
+				array(
+					'status' => false,
+					'error'  => esc_html__( 'You must be a Super User!', 'epsilon-framework' ),
+				)
+			);
+		}
+
+		return true;
+	}
+
+	/**
+	 * Send response
+	 *
+	 * @param array $args
+	 */
+	public function send_response( $args = array() ) {
+		wp_die( wp_json_encode( $args ) );
 	}
 }
